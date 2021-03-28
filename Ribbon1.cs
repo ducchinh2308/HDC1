664939040b6cb292bd9508d770fada4ad94218f7
ducchinh2308 <ducchinh2308@gmail.com>
2021-03-28 09:14:44 +0700

    Add project files.

---
+++ Ribbon1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace HDC1
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void btnTemplate_Click(object sender, RibbonControlEventArgs e)
+        {
+            string pathTemplate = @"C:\Users\Administrator\Documents\Custom Office Templates\HuynhDucChinh_Template.dotx"
+            cHDC_Function.ApplyTemplate(pathTemplate);
+        }
+    }
+}
---
+++ cHDC_Function.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HDC1
+{
+    class cHDC_Function
+    {
+        /// <summary>
+        /// apply template từ file template vào file hiện hành
+        /// </summary>
+        public static void ApplyTemplate(string pathFileTemplate)
+        {
+
+            Document thisdoc = Globals.ThisAddIn.Application.ActiveDocument;
+            //thisdoc.Activate();
+            string sName = thisdoc.FullName;
+            Range rng = thisdoc.Range();
+
+            rng.Copy();
+            object missing = Type.Missing;
+            Microsoft.Office.Interop.Word.Application app = new Microsoft.Office.Interop.Word.Application();
+            Microsoft.Office.Interop.Word.Document thatdoc = app.Documents.Open("@" + pathFileTemplate);
+            Range rng1 = thatdoc.Range();
+            rng1.InsertFile(sName);
+            //thatdoc.Content.Paste();
+        }
+    }
+}
